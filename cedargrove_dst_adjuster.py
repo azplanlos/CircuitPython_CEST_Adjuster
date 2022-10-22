dee559bf4ae07c4718f1f4bdffb38b0e9ef1838b
Cedar Grove Maker Studios <jan@goolsbey.net>
2022-10-22 12:57:03 -0700

    initial repo load

--- cedargrove_dst_adjuster.py
+++ cedargrove_dst_adjuster.py
@@ -1,4 +1,3 @@
-# SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 # SPDX-FileCopyrightText: Copyright (c) 2022 JG for Cedar Grove Maker Studios
 #
 # SPDX-License-Identifier: MIT
@@ -6,7 +5,8 @@
 `cedargrove_dst_adjuster`
 ================================================================================
 
-A CircitPython helper to adjust to North American Daylight Saving Time (DST).
+A CircuitPython helper to adjust North American Standard Time (xST) to Daylight
+Saving Time (DST).
 
 * Author(s): JG
 
@@ -34,25 +34,31 @@ def _detect_dst(datetime):
     returns ``True`` if the datetime object is within the DST window, ``False``
     if the datetime object is outside of the DST window. The helper cannot
     detect DST for a structured time object that was encoded as DST.
+
+    :param structured_time datetime: The Standard Time structured time
+    input value. Can be any structured time value within the specified date
+    calculation range of CircuitPython, currently January 1, 2000 00:00:00 to
+    January 19, 2038 03:14:07. No default value.
     """
 
     # Fix weekday and yearday structured time errors
     datetime = time.localtime(time.mktime(datetime))
 
-    # Convert Python time structure Monday-origin to Sunday-origin.
+    # Convert Python time structure from Monday-origin to Sunday-origin.
     weekday = (datetime.tm_wday + 1) % 7
 
     # Get the date of the previous Sunday or today's date if Sunday.
     prev_sunday_date = datetime.tm_mday - weekday
 
-    # March: Second Sunday occurs on the 8th through 14th of the month at
-    #   02:00 Standard Time (xST).
+    """ Test for March window opening threshold: Second Sunday occurs on the 8th
+    through 14th of the month at 02:00 Standard Time (xST).
+    """
     if datetime.tm_mon == 3:
         if prev_sunday_date <= 7:  # First Sunday of month or before
             return False  # xST
         if prev_sunday_date <= 14:  # Second Sunday of month
-            # determine current DST threshold
-            # year, March, previous Sunday date, 02 hours xST, 00 min, 00 sec
+            # Determine current DST threshold
+            #  year, March, previous Sunday date, 02 hours xST, 00 min, 00 sec
             dst_thresh = time.mktime(
                 time.struct_time(
                     (datetime.tm_year, 3, prev_sunday_date, 2, 0, 0, 0, -1, -1)
@@ -62,14 +68,16 @@ def _detect_dst(datetime):
                 return False  # xST
         return True  # DST
 
-    # November: First Sunday occurs on the 1st through 7th of the month at
-    #    01:00 Standard Time (xST) = 02:00 Daylight Saving Time (xDT)
+    """Test for November window closing threshold: First Sunday occurs on the
+    1st through 7th of the month at 01:00 Standard Time (xST) = 02:00 Daylight
+    Saving Time (xDT).
+    """
     if datetime.tm_mon == 11:
         if prev_sunday_date < 1:  # Before first Sunday of month
             return True  # DST
         if prev_sunday_date <= 7:  # First Sunday of month
-            # determine current xST threshold
-            # year, November, previous Sunday date, 01 hours xDT, 00 min, 00 sec
+            # Determine current xST threshold
+            #  year, November, previous Sunday date, 01 hours xDT, 00 min, 00 sec
             xst_thresh = time.mktime(
                 time.struct_time(
                     (datetime.tm_year, 11, prev_sunday_date, 1, 0, 0, 0, -1, -1)
@@ -79,20 +87,27 @@ def _detect_dst(datetime):
                 return True  # DST
         return False  # xST
 
-    # Check for Standard Time
+    # Check for Standard Time window
     if datetime.tm_mon < 3 or datetime.tm_mon > 11:  # Dec - Feb: xST
         return False  # xST
     return True  # DST; datetime.tm_mon is Apr - Oct
 
 
 def adjust_dst(datetime):
-    """Converts Standard Time to North American DST. Input to this function is
-    a structured time object in xST. The function returns a structured time
-    object adjusted to a DST value if appropriate and a flag indicating the DST
-    adjustment was made. The helper cannot detect DST for a structured time
-    object that is encoded as DST."""
+    """Converts North American Standard Time (xST) to Daylight Saving Time
+    (DST). Input to this function is a structured time object in xST. The
+    function returns a structured time object adjusted to a DST value if
+    appropriate and a flag indicating the DST adjustment was made. The helper
+    cannot correctly detect DST for a structured time object that is encoded as
+    DST.
 
-    is_dst = _detect_dst(datetime)  # Determine if datetime is DST
+    :param structured_time datetime: The Standard Time structured time
+    input value. Can be any structured time value within the specified date
+    calculation range of CircuitPython, currently January 1, 2000 00:00:00 to
+    January 19, 2038 03:14:07. No default value.
+    """
+
+    is_dst = _detect_dst(datetime)  # Determine if datetime is within DST window
 
     if is_dst:  # If DST, add an hour
         dst_date_time = time.mktime(datetime) + 3600
